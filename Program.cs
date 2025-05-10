5e4bd79ee32272596855651e0a7bc7941caf2732
YasmimAlves01 <yasmimalvesrodrigues16@gmail.com>
2025-05-10 18:08:46 -0300

    testando

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+double number;
+string decisao;
+
+Console.WriteLine("Bem vindo ao seu definidor de numero!");
+Console.WriteLine("Digite um numero e descubra se ele é par ou impar");
+
+number = Convert.ToDouble(Console.ReadLine());
+
+if (number % 2 == 0 ){
+    Console.WriteLine($"{number} é um numero par");
+}
+else{
+    Console.WriteLine($"{number} é um numero impar");
+}
+
+
+while (true)
+{
+Console.WriteLine("Deseja começar novamente?");
+Console.WriteLine("Digite S para sim e N para não");
+decisao = Console.ReadLine()!.ToUpper();
+
+if(decisao == "S"){
+    Console.WriteLine("Digite novamente um numero e descubra se ele é par ou impar");
+
+number = Convert.ToDouble(Console.ReadLine());
+
+if (number % 2 == 0 ){
+    Console.WriteLine($"{number} é um numero par");
+}
+else{
+    Console.WriteLine($"{number} é um numero impar");
+}
+
+}
+
+else{
+    Console.WriteLine("Muito obrigado por usar nossa aplicação ;)");
+    break;
+}
+}
